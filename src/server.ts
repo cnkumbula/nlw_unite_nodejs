7f4362bd9a29cdbb3968420ef91675b840aab660
cnkumbula <klaudio.kumbula@gmail.com>
2024-04-04 16:16:56 +0200

    ini

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import fastify from "fastify";
+
+import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-event";
+
+const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createEvent)
+app.register(registerForEvent)
+
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP server running on http://localhost:3333")
+})
